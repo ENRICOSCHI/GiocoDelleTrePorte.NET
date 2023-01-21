beab0f90b2af329a9a25a92ed1236793af100e18
ENRICOSCHI <113851247+ENRICOSCHI@users.noreply.github.com>
2023-01-21 20:59:22 +0100

    Aggiungere i file di progetto.

---
+++ GiocoDelleTrePorte.NET/Door.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiocoDelleTrePorte.NET
+{
+    internal class Door
+    {
+        Random rd = new Random();//initializze random
+        public int EliminateDoor(int NotEliminateThisDoor,int TheWinnerDoor)
+        {
+            int r = rd.Next(1, 4);// 1 2 3 
+            for (int i = 0;i<=100;i++)//useless I think?
+            {
+                
+                if(r== NotEliminateThisDoor || r== TheWinnerDoor)
+                {
+                    r=rd.Next(1,4);//repeat random choose
+                }
+                else
+                {
+                    break;//exit from for
+                }
+            }
+            return r;
+        }
+        public int remainedDoor(int n, int r)
+        {
+            int lastDoor = 0;
+            if(n==1 && r == 2|| n==2 &&r==1) // for the numeber 3
+            {
+                lastDoor = 3;
+            }
+            else if(n==1 && r == 3|| n==3 && r==1)// for the number 2
+            {
+                lastDoor = 2;
+            }
+            else
+            {
+                lastDoor = 1;// of course the last one is 1
+            }
+            return lastDoor;    
+        }
+        public int portaVincente()
+        {
+            int macchina=rd.Next(1, 4);//the door with the car behind
+            return macchina;
+        }
+    }
+}
---
+++ GiocoDelleTrePorte.NET/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GiocoDelleTrePorte.NET
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Door Porta = new Door();
+            int TheWinnerDoor = Porta.portaVincente();//saved the door winner
+            int EliminatedDoor=0;
+            Console.WriteLine("Buongiorno, questo a cui stai giocando viene chiamato il GIOCO DELLE TRE PORTE.\nIn questo gioco dovrai scegliere una tra le tre porte nelle quali ci potrebbe essere una macchina nuova di zecca oppure una capra.\nQuindi che porta scegli?\n 1\t\t 2\t\t 3\t\t\n"); //first 3 phrases that show up to the user
+            int FirstChosenDoor = int.Parse(Console.ReadLine());//user chose the door and it get saved on n
+            if (FirstChosenDoor == 1)
+            {
+               EliminatedDoor = Porta.EliminateDoor(FirstChosenDoor, TheWinnerDoor);
+            }
+            if(FirstChosenDoor == 2)
+            {
+                 EliminatedDoor=Porta.EliminateDoor(FirstChosenDoor, TheWinnerDoor);
+            }
+            if(FirstChosenDoor == 3)
+            {
+                 EliminatedDoor = Porta.EliminateDoor(FirstChosenDoor, TheWinnerDoor);
+            }
+            int ChangedDoor = Porta.remainedDoor(FirstChosenDoor, EliminatedDoor); //for know what is the last door remain between the two
+            Console.WriteLine("E' stata eliminata la porta numero: "  + EliminatedDoor.ToString() + "\nVolete cambiare porta con la numero:\t "+ ChangedDoor + " \to \tlasciare la scelta precedente?\nScrivere: c\t per cambiare\t o\t l\t per lasciare\n");//second phrase after had chose the door
+            string answer = Console.ReadLine();
+            if(answer == "c")//door changed answer
+            {
+                if (TheWinnerDoor == ChangedDoor)
+                {
+                    Console.WriteLine("HAI VINTO LA MACCHINA COMPLIMENTI!!\n");//win message
+                }
+                else
+                {
+                    Console.WriteLine("Peccato hai perso :( ; però hai vinto una capra :) .\n");//lost message
+                }
+                //add formula of percentage when you put it online in database
+            }
+            if (answer == "l")//No door changed answer
+            {
+                if(TheWinnerDoor==FirstChosenDoor)
+                {
+                    Console.WriteLine("HAI VINTO LA MACCHINA COMPLIMENTI!!\n");//win message
+                }
+                else
+                {
+                    Console.WriteLine("Peccato hai perso :( ; però hai vinto una capra :) .\n");//lost message
+                }
+                //add formula of percentage when you put it online in database
+            }
+            Console.WriteLine("Premere invio per finire.\n");//end message
+            Console.ReadLine();
+        }  
+    }
+}
